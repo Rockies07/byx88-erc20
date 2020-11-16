2cacf0af97e6bdffcb89831454b41e61ca0c2dc9
bllyanos <bllyanos@gmail.com>
2020-11-17 01:10:58 +0700

    deploy to kovan & mainnet

--- migrations/1605208460_deploycontracts.js
+++ migrations/1605208460_deploycontracts.js
@@ -15,9 +15,18 @@ module.exports = async function (deployer, network, accounts) {
     await deployer.deploy(TokenMock, web3.utils.toWei("10000000", "mwei"));
     const tm = await TokenMock.deployed();
     await deployer.deploy(Byx88Payment, tm.address);
+  } else if (network == "kovan") {
+    if (owner !== "0x3a0925aEe9856f9bc35751199643D24677542B6C") return;
+    await deployer.deploy(TokenMock, web3.utils.toWei("10000000", "mwei"));
+    const tm = await TokenMock.deployed();
+    await deployer.deploy(Byx88Payment, tm.address, {
+      from: "0x3a0925aEe9856f9bc35751199643D24677542B6C"
+    });
   } else {
-    if (owner !== "0xBEF473cE6738d73df0351107CC6f9A087CA0d00a") return;
+    if (owner !== "0x3a0925aEe9856f9bc35751199643D24677542B6C") return;
+
     // USDT - 0xdAC17F958D2ee523a2206206994597C13D831ec7
+
     const ERC20_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
     await deployer.deploy(Byx88Payment, ERC20_ADDRESS);
   }
